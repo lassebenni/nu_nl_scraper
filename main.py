34b4fe1c22e6f3f1f76a7e9f17ae81dd7635aba6
Lasse Benninga <lassebenninga@godatadriven.com>
2022-02-06 22:21:42 +0100

    Fix

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from models.headline import store_headlines
 PATH = "output/results.json"
 
 
-def main():
-    # headlines = parse_headlines(PATH)
-    headlines = scrape_headlines(PATH)
-    store_headlines(headlines)
-
-
 if __name__ == "__main__":
-    main()
+    headlines = scrape_headlines()
+    store_headlines(PATH, headlines)
